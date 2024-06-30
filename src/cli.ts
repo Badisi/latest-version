891bea27475521e402175c91e4eb55cc98388d8d
Badisi <badisi84@gmail.com>
2024-06-30 11:47:53 +0200

    fix: lint

--- src/cli.ts
+++ src/cli.ts
@@ -175,6 +175,7 @@ const displayTable = (updates: LatestVersionPackage[]): void => {
             ];
         };
 
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
         drawBox([
             '',
             (hasUpdates) ? yellow('Important updates are available.') : undefined,
