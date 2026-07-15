6bfff6b41e7c5504a908a139b07c66d9477d1c13
Badisi <badisi84@gmail.com>
2026-07-15 14:13:07 +0200

    fix: lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import hug from '@hug/eslint-config';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig(
+    hug.configs.stylistic.recommended,
     hug.configs.createModerate({
         noLoops: {
             rules: {
