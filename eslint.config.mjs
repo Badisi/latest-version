23af166ca65fae9ca9cec35c1fb4486966c0bf56
Badisi <badisi84@gmail.com>
2026-08-06 01:21:39 +0200

    refactor: improve lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,10 @@ import { defineConfig } from 'eslint/config';
 export default defineConfig(
     hug.configs.stylistic.recommended,
     hug.configs.createModerate({
+        angular: false,
+        rxjs: false,
+        rxjsAngular: false,
+        cypress: false,
         noLoops: {
             rules: {
                 'no-loops/no-loops': 'off',
